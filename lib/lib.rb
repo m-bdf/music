81049875dfbba1c1852a1cfd9aaf0dd19dbb064a
Maëlys Bras de fer <mae.bdf@outlook.com>
2024-07-02 01:28:24 +0000

    303

--- lib/lib.rb
+++ lib/lib.rb
@@ -1,5 +1,5 @@
 define :slide do |from, to|
-  return (line from, to, steps: 5, inclusive: true).ramp
+  return line from, to, steps: 5, inclusive: true
 end
 
 define :synths do |lead, *synths, &fn|
@@ -14,17 +14,23 @@ define :synths do |lead, *synths, &fn|
   fn.call
 end
 
-define :arrange do |song|
-  song.each do |pattern, playlist|
-    in_thread do
-      playlist.each do |v|
-        if v
-          send pattern, v
-        else
-          sleep 8
+define :arrange do |song, skip: 0|
+  song.each do |part|
+    part.each do |pattern, vs|
+      in_thread do
+        vs.drop(skip).each do |v|
+          if v
+            send pattern, v
+          else
+            sleep 8
+          end
         end
       end
     end
+    
+    max = part.values.map(&:length).max
+    sleep (max - skip) * 8
+    skip = 0
   end
 end
 
