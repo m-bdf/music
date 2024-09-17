7fa7a25a760d203bee71e7239745b155ad0abd7f
Maëlys Bras de fer <mae.bdf@outlook.com>
2024-09-17 23:41:23 +0200

    303 update

--- 303.rb
+++ 303.rb
@@ -1,5 +1,5 @@
 use_debug false
-use_bpm 170
+use_bpm 180
 use_synth :tb303
 
 eval_file "~/Documents/music/lib.rb"
@@ -8,26 +8,27 @@ eval_file "~/Documents/music/lib.rb"
 ### BASS
 
 define :bass do |v|
-  with_fx :wobble, res: 0 do
-    with_fx :octaver, amp: 0.5 do
+  with_fx :hpf, cutoff: v * 30 do
+    with_fx :wobble, res: 0 do
+      use_synth_defaults wave: 2, cutoff: 80, cutoff_slide: 1
+
       if v == 0
-        sleep 3
-        play :A0, wave: 1, cutoff: 80, attack: 2, release: 3
-        sleep 2
-        control note: :A1, cutoff: 100, cutoff_slide: 3
+        play :A0, attack: 4, release: 4
+        sleep 4
+        control note: :A1, cutoff: 110, cutoff_slide: 2
+        sleep 1
+        control note: :A0, note_slide: 3, amp: 0.5, wave: 0
         sleep 3
       else
-        play :A1, wave: 1, cutoff: 90, attack: 0, release: 3
+        play :A1, cutoff: 90, release: 3
         sleep 1
-        control note: :A0, cutoff: 120, cutoff_slide: 1
+        control note: :A0, amp: 0.5, wave: 1, cutoff: 110
         sleep 2
-
-        play :A0, cutoff: 80, sustain: 0.5, release: 0
+        play :A0, amp: 0.5, sustain: 0.5, release: 0
         sleep 1
-
-        play :A1, wave: 1, cutoff: 80, attack: 0, release: 4
+        play :A1, release: 4
         sleep 2
-        control note: :A0, cutoff: 100, cutoff_slide: 1
+        control note: :A0, amp: 0.5, wave: 1, cutoff: 100
         sleep 2
       end
     end
@@ -37,26 +38,36 @@ end
 
 ### DRUMS
 
-define :play_click do |**opts|
-  play :A2, wave: 1, attack: 0, release: 0.01, **opts
+define :play_click do
+  play :A2, wave: 1, attack: 0, release: 0.01
+end
+
+define :play_kick do
+  play_click
+  play :A1, wave: 2, cutoff: 30, release: 0.5
+end
+
+define :play_res do
+  play :A0, wave: 2, cutoff: 30, release: 0.1
 end
 
 define :kick do |v|
-  define :play_kick do
-    play_click
-    play :A1, wave: 2, cutoff: 30, release: 0.5
-  end
-
   with_fx :distortion, mix: v / 3.0 do
     with_fx :wobble, cutoff_max: 100 do
       with_fx :gverb, damp: 1 do
         8.times do
           play_kick
           sleep 0.5
-          play_kick if tick % 8 > 7 - v
-          sleep 0.25
-          play_click if look % 8 > 8 - v
-          sleep 0.25
+
+          if tick % 8 > 7 - v
+            play_kick
+            sleep 0.25
+            play_click
+            sleep 0.25
+          else
+            play_res
+            sleep 0.5
+          end
         end
       end
     end
@@ -64,16 +75,17 @@ define :kick do |v|
 end
 
 define :roll do |v|
-  with_fx :hpf, amp: 0.75 do
-    with_fx :slicer, wave: 2 do
+  with_fx :hpf, amp: 0.5 do
+    with_fx :slicer, wave: 3 do
       with_fx :reverb, room: v / 4.0 do
         with_fx :gverb, damp: 1 do
           8.times do
             play_click if tick % 4 < v
             sleep 0.5
-            play_click amp: 0.25
+            play_click
+            play_click
             sleep 0.25
-            play_click amp: 0.5 if look % 2 == 0
+            play_click if look % 2 == 0
             sleep 0.25
           end
         end
@@ -87,38 +99,42 @@ end
 
 define :lead do |v|
   with_fx :ping_pong, amp: 0.5 do
-    cutoff = 60 + v * 10
-    tick_reset(:cutoff)
+    with_fx :tanh, krunch: v / 12.0 do
+      tick_set 60 + v * 10
 
-    define :play_pick do |note|
-      play note, cutoff: cutoff + tick(:cutoff), release: 0.25
-      sleep 0.5
-    end
+      define :play_pick do |note|
+        play note, cutoff: tick, release: 0.25
+        sleep 0.5
+      end
 
-    define :play_slide do |from, to|
-      play from, amp: 0.5, cutoff: cutoff + tick(:cutoff)
-      sleep 0.5
-      control note: to, amp: 1, cutoff: (slide 60,100).tick
-      sleep 0.5
-    end
+      define :play_slide do |from, to|
+        play from, cutoff: tick
+        sleep v / 12.0
+        control note: to
+        sleep 1 - v / 12.0
+      end
 
-    play_slide :A3,:E3
-    play_slide :A3,:E4
-    play_slide :C4,:D4
-    play_pick :B3
-    play_pick :G3
+      if v == 0
+        play :A3, cutoff: tick, release: 4
+        sleep 4
+      else
+        play_slide :A3,:E3
+        play_slide :A3,:E4
+        play_slide :C4,:D4
+        play_pick :B3
+        play_pick :G3
+      end
 
-    if v % 4 == 0
-      play :A3, amp: 0.5, cutoff: cutoff + 5, release: 4
-      control amp: 1, cutoff: cutoff + 30, cutoff_slide: 2
-      sleep 4
-    else
-      play_slide :A3,:E4
-      play_slide :C4,:D4
-      play_slide :B3,:G3
-      play_pick :A3
-      play_pick :B3
-      print look
+      if v == 4
+        play :A3, cutoff: tick, release: 4
+        sleep 4
+      else
+        play_slide :A3,:E4
+        play_slide :C4,:D4
+        play_slide :B3,:G3
+        play_pick :A3
+        play_pick :B3
+      end
     end
   end
 end
@@ -128,15 +144,21 @@ end
 
 arrange [
   {
-    bass: [0, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,0,_, _,1,0,1, 1,1,1,0, 1,1,1,0],
-    kick: [_, _,_,0,0, 0,1,0,2, 0,1,2,1, 2,3,_,_, 0,1,0,2, 3,_,_,3, 2,0,2,1],
-    roll: [_, _,_,_,_, 1,0,2,0, 1,2,3,4, 0,_,_,1, 2,3,4,3, 4,_,3,4, 3,2,3,4],
-    lead: [_, _,_,_,_, _,_,_,_, _,_,_,_, _,_,0,1, 2,3,4,5, 6,6,5,4, 3,2,1,0],
+    bass: [0,1,1,1, 2,1,2,1, 2,1, 2,1,2,1],
+    kick: [_,_,_,_, 0,0,0,1, 0,2, 0,1,2,3],
+    roll: [_,_,4,2, _,_,1,0, 2,0, 1,2,3,4],
+    lead: [_,_,_,_, _,_,_,_, _,_, _,_,_,_],
   },
   {
-    bass: [1,1,0,_, _,1,0,1, 1,1,1,1, 0,1,1,0, 1,1,1,1, 1,1,1,0, 1,1,0],
-    kick: [2,3,_,_, 0,1,0,2, 3,_,_,3, 1,2,0,1, 2,3,_,_, 2,3,2,1, 2,3,_],
-    roll: [0,_,_,1, 2,3,4,3, 4,_,3,4, 2,3,1,2, 0,1,_,0, 1,2,3,4, 0,_,_],
-    lead: [_,_,0,1, 2,3,4,5, 6,6,5,6, 4,5,3,4, 2,3,1,2, 3,2,1,0, _,_,0],
+    bass: [3,0,2,1, 2,1,2,1, 2,1,2,1, 3,3],
+    kick: [_,_,_,_, 0,1,0,2, 0,1,2,3, _,_],
+    roll: [_,_,_,0, 1,0,2,0, 1,2,3,0, 2,4],
+    lead: [0,_,0,1, 2,3,2,4, 5,6,5,6, _,_],
+  },
+  {
+    bass: [1,2,1,2, 1,2,1,2, 1,2,1,2, 3,0],
+    kick: [3,2,1,0, 2,1,2,1, 2,1,0,_, 3,_],
+    roll: [4,3,2,1, 0,4,3,2, 1,0,4,_, 4,_],
+    lead: [6,5,6,4, 6,5,6,4, 3,2,1,0, _,0],
   },
 ]
