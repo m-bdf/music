4d0ba66e5a742cdde80374f242b041b12a62bf8b
Maëlys Bras de fer <mae.bdf@outlook.com>
2024-09-17 23:41:23 +0200

    303 pads

--- 303.rb
+++ 303.rb
@@ -1,6 +1,7 @@
 use_debug false
 use_bpm 180
 use_synth :tb303
+use_transpose 2
 
 eval_file "~/Documents/music/lib.rb"
 
@@ -8,28 +9,32 @@ eval_file "~/Documents/music/lib.rb"
 ### BASS
 
 define :bass do |v|
-  with_fx :hpf, cutoff: v * 30 do
-    with_fx :wobble, res: 0 do
-      use_synth_defaults wave: 2, cutoff: 80, cutoff_slide: 1
+  with_fx :compressor do
+    with_fx :rhpf, cutoff: (v + 1) * 20 do
+      with_fx :wobble, res: 0 do
+        use_synth_defaults wave: 2, cutoff: 80, cutoff_slide: 1
 
-      if v == 0
-        play :A0, attack: 4, release: 4
-        sleep 4
-        control note: :A1, cutoff: 110, cutoff_slide: 2
-        sleep 1
-        control note: :A0, note_slide: 3, amp: 0.5, wave: 0
-        sleep 3
-      else
-        play :A1, cutoff: 90, release: 3
-        sleep 1
-        control note: :A0, amp: 0.5, wave: 1, cutoff: 110
-        sleep 2
-        play :A0, amp: 0.5, sustain: 0.5, release: 0
-        sleep 1
-        play :A1, release: 4
-        sleep 2
-        control note: :A0, amp: 0.5, wave: 1, cutoff: 100
-        sleep 2
+        if v == 0
+          sleep 0.5
+          play :A0, attack: 4, release: 4
+          sleep 4
+          control note: :A1, cutoff: 110, cutoff_slide: 2
+          sleep 1
+          control note: :A0, note_slide: 3, amp: 0.5, wave: 0
+          sleep 2.5
+        else
+          sleep 0.5
+          play :A1, cutoff: 90, release: 3
+          sleep 1
+          control note: :A0, amp: 0.5, wave: 1, cutoff: 110
+          sleep 2
+          play :A0, amp: 0.5, sustain: 0.5, release: 0
+          sleep 1
+          play :A1, release: 4
+          sleep 2
+          control note: :A0, amp: 0.5, wave: 1, cutoff: 100
+          sleep 1.5
+        end
       end
     end
   end
@@ -44,29 +49,33 @@ end
 
 define :play_kick do
   play_click
-  play :A1, wave: 2, cutoff: 30, release: 0.5
+  play :A1, wave: 2, cutoff: 20, release: 0.5
 end
 
 define :play_res do
-  play :A0, wave: 2, cutoff: 30, release: 0.1
+  play :A0, wave: 2, cutoff: 40, release: 0.1
 end
 
 define :kick do |v|
-  with_fx :distortion, mix: v / 3.0 do
-    with_fx :wobble, cutoff_max: 100 do
-      with_fx :gverb, damp: 1 do
-        8.times do
-          play_kick
-          sleep 0.5
+  with_fx :compressor do
+    with_fx :rlpf, cutoff: 90 do
+      with_fx :distortion, mix: v / 3.0 do
+        with_fx :rhpf, cutoff: 30 do
+          with_fx :gverb, damp: 1 do
+            8.times do
+              play_kick
+              sleep 0.5
 
-          if tick % 8 > 7 - v
-            play_kick
-            sleep 0.25
-            play_click
-            sleep 0.25
-          else
-            play_res
-            sleep 0.5
+              if tick % 8 > 7 - v
+                play_kick
+                sleep 0.25
+                play_click
+                sleep 0.25
+              else
+                play_res if v == 0
+                sleep 0.5
+              end
+            end
           end
         end
       end
@@ -75,18 +84,19 @@ define :kick do |v|
 end
 
 define :roll do |v|
-  with_fx :hpf, amp: 0.5 do
+  with_fx :rhpf, amp: 0.25 do |rhpf|
     with_fx :slicer, wave: 3 do
       with_fx :reverb, room: v / 4.0 do
         with_fx :gverb, damp: 1 do
           8.times do
-            play_click if tick % 4 < v
+            at (line 0,1) do |t|
+              control rhpf, res: t, mix: t
+              play_click if t < v / 4.0
+            end if tick % 4 < v
+
             sleep 0.5
             play_click
-            play_click
-            sleep 0.25
-            play_click if look % 2 == 0
-            sleep 0.25
+            sleep 0.5
           end
         end
       end
@@ -97,25 +107,43 @@ end
 
 ### LEAD
 
+define :pads do |v|
+  with_fx :reverb, amp: 0.25 do
+    with_fx :slicer, phase: 0.5 do
+      play [:A4,:C4,:E4], wave: 2, sustain: 7
+      sleep 4
+      control notes: [:C4,:E4] if v > 0
+      sleep 2
+      control notes: [:B3,:D4] if v > 1
+      sleep 2
+    end
+  end
+end
+
 define :lead do |v|
-  with_fx :ping_pong, amp: 0.5 do
-    with_fx :tanh, krunch: v / 12.0 do
-      tick_set 60 + v * 10
+  with_fx :ping_pong, amp: 0.25 do
+    with_fx :tanh, krunch: v / 8.0 do
+      tick_reset
+
+      cutoff_min = look :cutoff
+      cutoff_max = tick_set :cutoff, 60.0 + v * 10
+      cutoff = range cutoff_min, cutoff_max.round(14)
+      cutoff = [cutoff_max] if cutoff.length != 10
 
       define :play_pick do |note|
-        play note, cutoff: tick, release: 0.25
+        play note, cutoff: cutoff.tick, release: 0.25
         sleep 0.5
       end
 
       define :play_slide do |from, to|
-        play from, cutoff: tick
-        sleep v / 12.0
+        play from, cutoff: cutoff.tick
+        sleep v / 8.0
         control note: to
-        sleep 1 - v / 12.0
+        sleep 1 - v / 8.0
       end
 
       if v == 0
-        play :A3, cutoff: tick, release: 4
+        play :A3, cutoff: cutoff.tick, release: 4
         sleep 4
       else
         play_slide :A3,:E3
@@ -126,7 +154,7 @@ define :lead do |v|
       end
 
       if v == 4
-        play :A3, cutoff: tick, release: 4
+        play :A3, cutoff: cutoff.tick, release: 4
         sleep 4
       else
         play_slide :A3,:E4
@@ -144,21 +172,24 @@ end
 
 arrange [
   {
-    bass: [0,1,1,1, 2,1,2,1, 2,1, 2,1,2,1],
-    kick: [_,_,_,_, 0,0,0,1, 0,2, 0,1,2,3],
-    roll: [_,_,4,2, _,_,1,0, 2,0, 1,2,3,4],
-    lead: [_,_,_,_, _,_,_,_, _,_, _,_,_,_],
+    bass: [_,0, _,0,_,1, _,2,1, _,1,2,3,2, 1,2,3,2, 1,2,_,_],
+    kick: [_,_, 0,0,0,1, 0,2,1, 0,1,2,1,2, 0,1,2,3, 2,3,_,_],
+    roll: [_,_, _,_,2,0, 1,0,1, 2,1,2,0,1, 2,1,0,_, 3,4,_,_],
+    pads: [0,_, 0,_,0,_, 1,0,_, 2,1,2,0,_, 2,1,0,_, 1,0,2,0],
+    lead: [_,_, _,_,_,_, _,_,_, _,_,_,_,_, _,_,_,_, _,_,_,_],
   },
   {
-    bass: [3,0,2,1, 2,1,2,1, 2,1,2,1, 3,3],
-    kick: [_,_,_,_, 0,1,0,2, 0,1,2,3, _,_],
-    roll: [_,_,_,0, 1,0,2,0, 1,2,3,0, 2,4],
-    lead: [0,_,0,1, 2,3,2,4, 5,6,5,6, _,_],
+    bass: [3,0,2,1, 2,1,3,1, 2,1,3,1, 2,1,3,1, 2,1,3,1, _,0],
+    kick: [_,_,_,_, 0,1,0,2, 0,1,2,_, 0,1,2,3, 2,3,2,1, _,_],
+    roll: [_,_,2,0, 1,0,2,0, 1,2,1,0, 1,2,3,0, 3,2,1,0, _,4],
+    pads: [_,_,_,_, _,_,0,_, 1,_,2,_, 1,0,2,0, 2,1,2,0, _,_],
+    lead: [0,_,0,1, 2,3,2,4, 5,6,5,6, 3,2,1,0, 6,5,6,5, 4,_],
   },
   {
-    bass: [1,2,1,2, 1,2,1,2, 1,2,1,2, 3,0],
-    kick: [3,2,1,0, 2,1,2,1, 2,1,0,_, 3,_],
-    roll: [4,3,2,1, 0,4,3,2, 1,0,4,_, 4,_],
-    lead: [6,5,6,4, 6,5,6,4, 3,2,1,0, _,0],
+    bass: [1,3,1,2, 1,3,1,2, 1,3,2,1, 1,3,1,2, 1,3,2,1, 3,0],
+    kick: [3,2,1,0, 2,1,2,1, 2,1,0,_, 2,1,2,1, 2,1,0,_, 3,_],
+    roll: [3,2,1,0, 3,2,1,0, 2,0,1,0, 3,2,1,0, 2,0,1,0, 4,_],
+    pads: [2,1,2,0, 2,1,0,_, 2,_,1,_, 2,1,0,_, 2,_,1,_, 0,_],
+    lead: [6,5,6,4, 5,6,5,6, 3,2,1,0, 5,6,5,6, 3,2,1,0, _,0],
   },
 ]
