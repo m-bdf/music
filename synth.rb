ac4151ab01492bf1e43c5e3964b32a5073e42f35
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-11-12 14:35:01 +0100

    updates

--- synth.rb
+++ synth.rb
@@ -1,7 +1,7 @@
 use_debug false
 use_bpm 150
 
-run_file "~/Documents/music/lib.rb"
+eval_file "~/Documents/music/lib.rb"
 
 kick = load_sample :bd_tek
 
@@ -12,13 +12,13 @@ define :bass do |v|
   with_fx :ping_pong do
     use_synth :dsaw
     use_synth_defaults release: 0.5
-    
+
     8.times do
       n = [
         [:A1,:G1,:G1,:G1,:F1,:E1,:E1,:E1],
         [:A1,:C2,:C2,:C2,:B1,:F1,:F1,:F1],
       ][v].tick
-      
+
       play n
       sleep 0.7
       play n, amp: 0.7
@@ -35,7 +35,7 @@ define :kick do |v|
     use_synth :gabberkick
     use_octave v
     play :A1, sustain: 0.25
-    
+
     sample kick
     sleep 0.4
     sample kick, amp: 0.4
@@ -43,7 +43,7 @@ define :kick do |v|
     sample kick, amp: 0.7
     sleep 0.3
   end
-  
+
   with_fx :distortion do
     if v == 0
       play_kick
@@ -65,11 +65,11 @@ define :lead do |v|
       [:A3,:G4,:G4,:G4,:G4,:E4,:F4,:E4,:E4,:E4,:E4,:C4],
       [:A3,:C4,:C4,:C4,:C4,:A3,:B3,:F4,:F4,:F4,:E4,:C4],
     ][v % 2]
-    
+
     t = [1,0.4,0.3,0.3,1,1,1,0.4,0.3,0.3,1,1]
-    
+
     use_synth_defaults ramp_ratio: 0
-    
+
     synths :winwood_lead, :supersaw do
       play_pattern_timed n, t, sustain: 0.5
     end
@@ -84,5 +84,5 @@ arrange [
     lead: [1,0,0,0,1,0,0,0,1,0,0,0,1],
     bass: [_,0,0,0,1,0,0,0,1,0,0,0,1],
     kick: [0,1,1,1,0,1,1,1,0,1,1,1,0],
-  }
+  },
 ]
