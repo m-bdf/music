a7934aab3aa6d372fd34a05c04e57032c3af14f3
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-12-16 06:58:58 +0100

    Working mondo export

--- mondo.mjs
+++ mondo.mjs
@@ -1,39 +1,48 @@
 import fs from 'node:fs';
+import wav from 'node-wav';
 
 import * as core from '@strudel/core';
+import { Dough } from 'supradough';
 import { getAudioContext } from 'superdough';
 import { webaudioRepl } from '@strudel/webaudio';
 import { mondo } from '@strudel/mondo';
-
-import encodeWav from 'audiobuffer-to-wav';
 
 const file = process.argv[2];
 const code = fs.readFileSync(file, 'utf8');
 const duration = +process.argv[3];
 
-await core.evalScope(core,
-  core.registerControl('markcss'),
-  await import('node-web-audio-api'),
-);
+await core.evalScope(core, core.registerControl('markcss'));
 
 if (duration) {
-  AudioContext = class extends OfflineAudioContext {
-    constructor(sampleRate = 44100) {
-      super(2, sampleRate * duration, sampleRate);
-    }
-  };
-}
+  const dough = new Dough();
 
-const ctx = getAudioContext();
-const repl = webaudioRepl();
+  for (const hap of mondo(code).queryArc(0, duration)) {
+    hap.value._begin = hap.whole.begin;
+    hap.value._duration = hap.duration;
+    dough.scheduleSpawn(hap.value);
+  }
 
-if (duration) {
-  repl.setPattern(mondo(code), false);
-  const buf = encodeWav(await ctx.startRendering());
-  fs.writeFileSync(`${file}.wav`, Buffer.from(buf));
+  const data = [
+    new Float32Array(dough.sampleRate * duration),
+    new Float32Array(dough.sampleRate * duration),
+  ];
+
+  while (dough.t < data[0].length) {
+    dough.update();
+    data[0][dough.t - 1] = dough.out[0];
+    data[1][dough.t - 1] = dough.out[1];
+  }
+
+  const buf = wav.encode(data, dough);
+  fs.writeFileSync(`${file}.wav`, buf);
 }
 
 else {
-  await ctx.audioWorklet.addModule('supradough/dough-worklet.mjs');
+  await evalScope(import('node-web-audio-api'));
+  await getAudioContext().audioWorklet
+    .addModule('supradough/dough-worklet.mjs');
+
+  const repl = webaudioRepl();
+  repl.setCps(1);
   repl.setPattern(mondo(code).supradough());
 }
