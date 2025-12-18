1705b9576530b7fc59c241441f42747c92eb2a0b
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-12-18 04:28:39 +0100

    Support samples loading

--- mondo.mjs
+++ mondo.mjs
@@ -2,21 +2,30 @@ import fs from 'node:fs';
 import wav from 'node-wav';
 
 import * as core from '@strudel/core';
-import { Dough } from 'supradough';
-import { getAudioContext } from 'superdough';
-import { webaudioRepl } from '@strudel/webaudio';
 import { mondo } from '@strudel/mondo';
+import { Dough } from 'supradough';
+import {
+  getAudioContext,
+  doughsamples,
+  webaudioRepl,
+} from '@strudel/webaudio';
 
 const file = process.argv[2];
 const code = fs.readFileSync(file, 'utf8');
 const duration = +process.argv[3];
 
-await core.evalScope(core, core.registerControl('markcss'));
+await core.evalScope(core, core.registerControl('markcss'), {
+  samples: url => pure({ promise: doughsamples(url.__pure) }),
+});
+
+let pat = mondo(code);
+await Promise.all(pat.firstCycleValues.map(v => v.promise));
+pat = pat.filterValues(v => !v.promise);
 
 if (duration) {
   const dough = new Dough();
 
-  for (const hap of mondo(code).queryArc(0, duration)) {
+  for (const hap of pat.queryArc(0, duration)) {
     hap.value._begin = hap.whole.begin;
     hap.value._duration = hap.duration;
     dough.scheduleSpawn(hap.value);
@@ -44,5 +53,5 @@ else {
 
   const repl = webaudioRepl();
   repl.setCps(1);
-  repl.setPattern(mondo(code).supradough());
+  repl.setPattern(pat.supradough());
 }
