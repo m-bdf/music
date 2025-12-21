5186d71514070df646fd1f75881589a61f1cf80f
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-12-21 06:49:04 +0100

    Switch from supra to super dough

--- mondo.mjs
+++ mondo.mjs
@@ -8,21 +8,30 @@ import wavEncode from 'audiobuffer-to-wav';
 import * as core from '@strudel/core';
 import * as tonal from '@strudel/tonal';
 import { mondo } from '@strudel/mondo';
-import { doughsamples } from '@strudel/webaudio';
-import { setAudioContext, getAudioContext } from 'superdough';
-import { workletUrl } from 'supradough';
 
 const src = process.argv[2];
 const code = fs.readFileSync(src, 'utf8');
 const out = process.argv[3];
 
-await core.evalScope(core, tonal, webAudio, { window: globalThis });
+await core.evalScope(webAudio);
+
+const {
+  samples,
+  registerSynthSounds,
+  webaudioOutput,
+  setAudioContext,
+  getAudioContext,
+} = await import('@strudel/webaudio');
+
+await core.evalScope(core, tonal, { window: globalThis });
 
 strudelScope.samples = url => pure({
-  fetch: () => doughsamples(url.__pure),
+  fetch: () => samples(url.__pure),
 });
 
 registerControl('markcss');
+
+await registerSynthSounds();
 
 let pat = mondo(code);
 await Promise.all(pat.firstCycleValues.map(v => v.fetch?.()));
@@ -31,6 +40,7 @@ pat = pat.filterValues(v => !v.fetch);
 const repl = core.repl({
   getTime: () => !out ? audioCtx.currentTime :
     repl.scheduler.clock.getPhase() - repl.scheduler.clock.minLatency,
+  defaultOutput: webaudioOutput,
 });
 
 repl.setCps(1);
@@ -83,35 +93,8 @@ else if (process.stderr.isTTY) {
 }
 
 const audioCtx = getAudioContext();
-
-let doughWorklet = `import('${workletUrl}')`;
-
-if (out) {
-  doughWorklet += `
-    import { workerData } from 'node:worker_threads';
-
-    import nativeAudio from '${
-      import.meta.resolve('node-web-audio-api')
-    }/../load-native.cjs';
-
-    const wrapped = registerProcessor;
-
-    registerProcessor = (name, Processor) =>
-      wrapped(name, class extends Processor {
-        process(...args) {
-          if (super.process(...args) && this.dough.t >= ${audioCtx.length}) {
-            nativeAudio.exit_audio_worklet_global_scope(workerData.workletId);
-          }
-        }
-      });
-  `;
-}
-
-const doughWorkletUrl = URL.createObjectURL(new Blob([doughWorklet]));
-await audioCtx.audioWorklet.addModule(doughWorkletUrl);
-URL.revokeObjectURL(doughWorkletUrl);
-
-await repl.setPattern(pat.supradough());
+await audioCtx.audioWorklet.addModule('superdough/worklets.mjs');
+await repl.setPattern(pat);
 
 if (out) {
   await scheduler.wait(1000);
