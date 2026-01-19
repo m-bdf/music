9bd457ab158d5b7f58eaf802370003d4225542ef
Maëlys Bras de fer <mae.bdf@outlook.com>
2026-01-19 14:04:21 +0100

    Add vscode extension

--- flake.nix
+++ flake.nix
@@ -4,15 +4,22 @@
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    sonic-pi-tool = {
-      url = "github:Sohalt/sonic-pi-tool";
-      inputs.utils.follows = "flake-utils";
+    vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    strudel-mondo = {
+      # printf 'e/%s\nts/%s' '@strudel/core' 'mondo' | base64
+      url = "https://esm.sh/v135/@strudel/mondo/X-ZS9Ac3RydWRlbC9jb3JlCnRzL21vbmRv/node/mondo.bundle.mjs";
+      flake = false;
     };
   };
 
   outputs = inputs:
     inputs.flake-utils.lib.eachDefaultSystem (system:
-      with inputs.nixpkgs.legacyPackages.${system};
+      with inputs.nixpkgs.legacyPackages.${system}.extend
+        inputs.vscode-extensions.overlays.default;
     {
       packages.mondo = writeShellScriptBin "mondo" ''
         LD_LIBRARY_PATH+=:"${alsa-lib}/lib"
@@ -33,11 +40,26 @@
         ${lib.getExe nodejs_latest} mondo.mjs "$src" "''${out%/}"
       '';
 
-      devShells.sonic-pi = mkShellNoCC {
-        packages = [
-          sonic-pi
-          inputs.sonic-pi-tool.packages.${system}.default
-        ];
+      packages.mondo-vscode =
+        vscode-marketplace.cmillsdev.strudelvs.overrideAttrs {
+          patchPhase = ''
+            sed '/@strudel\/core/{ s| as |:|g; s|*as||g
+              s|import|var|g; s|from[^;]*|=globalThis|g
+            }' ${inputs.strudel-mondo} > dist/mondo.js
+
+            echo '
+              registerControl("markcss");
+              strudelScope.samples = url => pure({
+                fetch: () => doughsamples(url.__pure),
+              });
+            ' >> dist/mondo.js
+
+            sed -i '/repl.evaluate(tune)/c (async () => { \
+              const pat = (await import("./mondo.js")).mondo(tune); \
+              await Promise.all(pat.firstCycleValues.map(v => v.fetch?.())); \
+              await repl.setPattern(pat.filterValues(v => !v.fetch)); \
+            })();' dist/strudel.js
+          '';
       };
     });
 }
